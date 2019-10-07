5c30e1eda18ce635982679e53f54fc6b387cb3dd
ab100777 <ab100777@gmail.com>
2019-10-07 21:37:59 +0800

    no message

---
+++ src/components/Customerboard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <Alert></Alert>
+    <div style="background-color:rgb(43, 42, 65);padding-top:40px;padding-bottom:1px;font-family: Noto Sans TC">
+      <router-view></router-view>
+    </div>
+    <footer style="background-color:rgb(43, 42, 65);" class>
+      <div style="display:flex" class="justify-content-center">
+        <img src="./img/logo_steam_footer.png" alt />
+      </div>
+      <div class="text-info text-center mt-3">資料、圖片來源皆來自網路，僅用來做為學習用途。</div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+import Alert from "./AlertMessage";
+
+export default {
+  components: {
+    Navbar,
+    Alert
+  }
+};
+</script>
+
+<style scoped>
+footer {
+  padding-top: 400px;
+  padding-bottom: 30px;
+}
+
+@media (max-width: 576px) {
+  footer {
+    padding-top: 30px;
+    padding-bottom: 30px;
+  }
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-md navbar-light">
+      <div class="container">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <router-link class="nav-link" to="/CustomerOrders">
+          <img src="./img/Logo-Steam.png" alt width="150px" class="navbar-logo" />
+        </router-link>
+        <div class="collapse navbar-collapse order-sm-1" id="navbarNav">
+          <ul class="navbar-nav ml-auto" style="font-weight: bolder;font-size:15px">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">
+                <span data-feather="file-text"></span>
+                <i class="fas fa-home"></i>
+                首頁
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/CustomerOrders">
+                <span data-feather="file-text"></span>
+                <i class="fas fa-box-open"></i>商城
+              </router-link>
+            </li>
+            <!-- <li class="nav-item" v-for="item in cart.carts" :key="item.id"> -->
+              <li class="nav-item">
+              <router-link class="nav-link" to="/cart">
+                <span data-feather="file-text"></span>
+                <i class="fas fa-shopping-cart"></i>
+                購物車
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  // data() {
+  //   return {
+  //     cart: {}
+  //   };
+  // },
+  methods: {
+    signout() {
+      const vm = this;
+      const url = `${process.env.APIPATH}/logout`;
+      this.$http.post(url).then(response => {
+        console.log(response.data);
+        if (response.data.success) {
+          vm.$router.push("/loginin");
+        }
+      });
+    },
+    // getCart() {
+    //   const vm = this;
+    //   const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+    //   this.$http.get(url).then(response => {
+    //     vm.cart = response.data.data;
+    //     console.log(response);
+    //   });
+    // }
+  },
+  // created() {
+  //   this.getCart();
+  // }
+};
+</script>
